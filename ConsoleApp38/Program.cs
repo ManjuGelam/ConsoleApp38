0058d9742af4be47e563c5c60f666ffb9bc901a8
APTOL301938 <APTOL301938@APOLBO03-188>
2023-03-30 17:42:44 +0530

    Add project files.

---
+++ ConsoleApp38/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp38
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int[] ab = { 10, 23, 50, 20, 55 };
+            Console.WriteLine("element are divisible by 10 in array is");
+            for (int i = 0; i < ab.Length; i++)
+            {
+                if (ab[i] % 10 == 0)
+                {
+                    Console.Write(ab[i] + " ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
